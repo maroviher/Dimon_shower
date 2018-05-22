262efb215628b3db8b3de40e169f8dd03fa8d1c8
maroviher <qscgy@mail.ru>
2018-05-22 22:39:37 +0200

    audio works finally

--- app/src/main/java/com/example/rmoschenski/dimon_shower/AudioIncoming.java
+++ app/src/main/java/com/example/rmoschenski/dimon_shower/AudioIncoming.java
@@ -21,8 +21,52 @@ public class AudioIncoming {
     MediaCodec mMediaCodecAudioDecoder;
     AudioTrack mAudioTrack;
     public BlockingQueue<Integer> mFifoAudioIn = new LinkedBlockingQueue<Integer>();
-    int m_iPCM_DataWrote = 0, m_iNotificationPeriod, m_iSampleRate;
+    int m_iPCM_DataWrote = 0, m_iSampleRate;
     TextView m_messageView;
+
+    int iOldSampleRate = m_iSampleRate;
+
+    int iLastPCM_DataWrote = 0;
+    long l_ms = 0;
+    void Adjust() {
+        int i_l = m_iSampleRate/14, i_m = m_iSampleRate/12, i_r = m_iSampleRate/10, i_congestion = m_iSampleRate/4;
+        double adjust = 1.003;
+        int iNewSampleRate = 0;
+        int m_iDelta = m_iPCM_DataWrote - mAudioTrack.getPlaybackHeadPosition();
+        if(m_iDelta >= i_congestion) {//we have a real congestion, play it quick
+            iNewSampleRate = (int) (m_iSampleRate + m_iSampleRate*(float)(m_iDelta)/m_iSampleRate);
+        }
+        else if(m_iDelta >= i_r) {//play quicker
+            iNewSampleRate = (int) (m_iSampleRate * adjust);
+        }
+        else if((m_iDelta < i_m) && (m_iDelta >= i_l)) {//play with native speed
+            iNewSampleRate = m_iSampleRate;
+        }
+        else if(m_iDelta < i_l) {//play slower
+            iNewSampleRate = (int) (m_iSampleRate / adjust);
+        }
+
+        if(iOldSampleRate != iNewSampleRate) {
+            mAudioTrack.setPlaybackRate(iNewSampleRate);
+            iOldSampleRate = iNewSampleRate;
+        }
+        boolean bStatistics = true;
+        if(bStatistics) {
+            long time_delta_ms = System.currentTimeMillis() - l_ms;
+            int i_msBetweenMeasure = 200;
+            if (time_delta_ms >= i_msBetweenMeasure) {
+                int pcm_delta = m_iPCM_DataWrote - iLastPCM_DataWrote;
+                float iPCMperSec = pcm_delta * (float) 1000 / time_delta_ms;
+                iLastPCM_DataWrote = m_iPCM_DataWrote;
+
+                m_messageView.setText("PCM/s=" + (int) iPCMperSec + " underrun=" + mAudioTrack.getUnderrunCount() +
+                        " delta=" + m_iDelta + " rate=" + mAudioTrack.getPlaybackRate());
+                /*Log.d("asdf", "pcm_delta=" + pcm_delta + " iPCMperSec=" + iPCMperSec + " delta=" + m_iDelta +
+                        " head=" + mAudioTrack.getPlaybackHeadPosition() + " rate=" + mAudioTrack.getPlaybackRate());*/
+                l_ms += time_delta_ms;
+            }
+        }
+    }
 
     public void Start(TextView messageView, int iAECid, int iSampleRate) {
         m_iSampleRate = iSampleRate;
@@ -30,58 +74,14 @@ public class AudioIncoming {
         Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
         mAudioTrack = new AudioTrack(AudioManager.MODE_IN_COMMUNICATION, iSampleRate,
                 AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_FLOAT, iSampleRate*2,
+                AudioFormat.ENCODING_PCM_16BIT, iSampleRate*2/2, //0.5 sec
                 AudioTrack.MODE_STREAM, iAECid);
-
-        m_iNotificationPeriod = iSampleRate/2;
-        mAudioTrack.setPositionNotificationPeriod(m_iNotificationPeriod);//onPeriodicNotification gets called every 500ms
-        mAudioTrack.setPlaybackPositionUpdateListener(new AudioTrack.OnPlaybackPositionUpdateListener() {
-            @Override
-            public void onMarkerReached(AudioTrack audioTrack) {}
-long time = System.currentTimeMillis();
-            int iOldSampleRate = m_iSampleRate;
-            @Override
-            public void onPeriodicNotification(AudioTrack audioTrack) {
-
-                int i_l = m_iSampleRate/10, i_m = m_iSampleRate/8, i_r = m_iSampleRate/6, i_congestion = m_iSampleRate/4;
-                double adjust = 1.003;
-                int iNewSampleRate = 0;
-                int m_iDelta = m_iPCM_DataWrote - mAudioTrack.getPlaybackHeadPosition();
-                if(m_iDelta >= i_congestion) {//we have a real congestion, play it quick
-                    iNewSampleRate = (int) (m_iSampleRate * 2);
-                }
-                else if(m_iDelta >= i_r) {//play quicker
-                    iNewSampleRate = (int) (m_iSampleRate * adjust);
-                }
-                else if((m_iDelta < i_m) && (m_iDelta >= i_l)) {//play with native speed
-                    iNewSampleRate = m_iSampleRate;
-                }
-                else if(m_iDelta < i_l) {//play slower
-                    iNewSampleRate = (int) (m_iSampleRate / adjust);
-                }
-                iNewSampleRate = (int) (m_iSampleRate * 2);
-                if(iOldSampleRate != iNewSampleRate) {
-                    mAudioTrack.setPlaybackRate(iNewSampleRate);
-                    iOldSampleRate = iNewSampleRate;
-                }
-                double mytime1 = System.currentTimeMillis();
-                m_messageView.setText((System.currentTimeMillis() - time) + "ms getUnderrunCount=" + mAudioTrack.getUnderrunCount() + " delta=" + m_iDelta + " rate="+mAudioTrack.getPlaybackRate());
-                //Log.d("", "ms="+(mytime1 - mytime)+" getUnderrunCount=" + mAudioTrack.getUnderrunCount() + " delta=" + m_iDelta + " rate="+mAudioTrack.getPlaybackRate());
-                time = System.currentTimeMillis();
-            }
-          });
+        mAudioTrack.play();
 
         if(null == (mMediaCodecAudioDecoder = InitAudioDecoder(iSampleRate)))
             return;
 
-        mAudioTrack.play();
         mMediaCodecAudioDecoder.start();
-    }
-
-    public void IncPcmDataCount(int i) {
-        synchronized (this) {
-            m_iPCM_DataWrote += i;
-        }
     }
 
     public void Stop() {
@@ -116,53 +116,17 @@ long time = System.currentTimeMillis();
                         e.printStackTrace();
                     }
                 }
-int tt=0;long time_left, time_right;
-boolean bInit = true;
+                int iOutputBufferCounter = 0;
                 @Override
                 public void onOutputBufferAvailable(@NonNull MediaCodec mediaCodec, int inputBufferId, @NonNull MediaCodec.BufferInfo bufferInfo) {
                     ByteBuffer buf = mediaCodec.getOutputBuffer(inputBufferId);
                     //Log.d("onOutputBufferAvailable", "id=" + inputBufferId + " size=" +  buf.limit());
                     int iBufSize = buf.limit();
-                    tt+=iBufSize;
+                    int iWrote = mAudioTrack.write(buf, iBufSize, AudioTrack.WRITE_NON_BLOCKING);
+                    m_iPCM_DataWrote += iWrote/2;
+                    if(0 == (iOutputBufferCounter++ % 5))
+                        Adjust();
 
-                    if(bInit) {
-                        time_left = System.currentTimeMillis();
-                        bInit = false;
-                    }
-
-                    long delta_ms = System.currentTimeMillis()-time_left;
-
-                    if(delta_ms > 1000) {
-                        Log.d("", "Bytes=" + tt + " in " + delta_ms + "ms");
-                        tt = 0;
-                        time_left = System.currentTimeMillis();
-                    }
-
-                    float[] buf_sin = new float[iBufSize/2];
-                    byte[] buf_sin_byte = new byte[iBufSize];
-
-                    double sin_max = Math.PI*2;
-
-                    double step=(sin_max)/buf_sin.length;
-                    int i = 0, ii=0;
-                    for(double x = 0; x < (sin_max*0.999); x+=step) {
-                        //str += ""+(double)(Math.sin(Math.toRadians(x))*1)+" ";
-                        try {
-                            buf_sin[i++] = (float)Math.sin(x);
-                            int i_sample = (int)(32767*(1+buf_sin[i-1]));
-                            buf_sin_byte[ii+0]   = (byte)(i_sample & 0xFF);
-                            buf_sin_byte[ii+1] = (byte)((i_sample & 0xFF00)>>8);
-                            ii+=2;
-
-                            /*if(i%10== 0)
-                                Log.d("", String.format("%05d=0x%02x%02x", i_sample, buf_sin_byte[ii-2], buf_sin_byte[ii-1]));*/
-                        } catch (ArrayIndexOutOfBoundsException ssd) {
-                            Log.d("", "");
-                        }
-                    }
-                    mAudioTrack.write(buf_sin, 0, buf_sin.length, AudioTrack.WRITE_NON_BLOCKING);
-                    //mAudioTrack.write(buf, iBufSize, AudioTrack.WRITE_NON_BLOCKING);
-                    IncPcmDataCount(iBufSize/2);
                     mediaCodec.releaseOutputBuffer(inputBufferId, true);
                 }
 
